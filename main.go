8806647d8300653e497016e7648269138e5fa60d
Daniel Bergqvist <dbergqvist@gmail.com>
2018-11-02 10:19:48 +0100

    Added weather data

--- main.go
+++ main.go
@@ -1,12 +1,55 @@
 package main
 
-import "net/http"
+import (
+    "net/http"
+    "encoding/json"
+    "strings"
+)
 
 func main() {
-    http.HandleFunc("/", hello)
+    http.HandleFunc("/hello", hello)
+
+    http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+        city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+        data, err := query(city)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Set("Content-Type", "application/json; charset=urf-8")
+        json.NewEncoder(w).Encode(data)
+    })
+    
     http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hello!"))
+    w.Write([]byte("hello!\n"))
+}
+
+func query(city string) (weatherData, error) {
+    resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=81978a58f67c761155e5650662deedfc&q=" + city)
+    if err != nil {
+        return weatherData{}, err
+    }
+
+    defer resp.Body.Close()
+    
+    var d weatherData
+    
+    if err := json.NewDecoder(resp.Body).Decode(&d); err != nil{
+        return weatherData{}, err
+    }
+    
+    return d, nil
+    
+}
+
+type weatherData struct {
+    Name string `json:"name"`
+    Main struct {
+        Kelvin float64 `json:"temp"` 
+    } `json:"main"`
 }
